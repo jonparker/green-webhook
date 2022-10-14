cdcac667d065c70302ea3952a3cea0ee2b704823
Jon Parker <jonathanparkeremail@gmail.com>
2022-10-14 16:39:30 +1100

    feat: added forecast test.

--- index.mjs
+++ index.mjs
@@ -1,11 +1,12 @@
 import fetch from "node-fetch";
 
+const baseUri = "https://carbon-aware-api.azurewebsites.net";
+
 // Calculate the best emission data by list of locations for a specified time period.
 async function BestEmissionData(locations, fromTime, toTime) {
   if (locations.length === 0) {
-    return [];
+    return null;
   }
-  const baseUri = "https://carbon-aware-api.azurewebsites.net";
   const locationsQuery = locations
     .map((location) => `location=${location}`)
     .join("&");
@@ -15,10 +16,31 @@ async function BestEmissionData(locations, fromTime, toTime) {
   return await res.json();
 }
 
-const res = await BestEmissionData(
+async function CurrentForecast(locations, fromTime, toTime, windowSize) {
+  if (locations.length === 0) {
+    return null;
+  }
+  const locationsQuery = locations
+    .map((location) => `location=${location}`)
+    .join("&");
+  const res = await fetch(
+    `${baseUri}/emissions/forecasts/current?${locationsQuery}&dataStartAt=${fromTime}&dataEndAt=${toTime}&windowSize=${windowSize}`
+  );
+  return await res.json();
+}
+
+const test1 = await BestEmissionData(
   ["eastus", "westus"],
   "2022-03-01T15:30:00Z",
   "2022-03-01T18:30:00Z"
 );
+console.log(`Best emissions`, test1);
 
-console.log(res);
+const test2 = await CurrentForecast(
+  ["eastus", "westus"],
+  "2022-10-14T06:00:00Z",
+  "2022-10-14T07:00:00Z",
+  30
+);
+
+console.log(`Current forecast`, test2);
