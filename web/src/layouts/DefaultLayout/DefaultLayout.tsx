2e3176c0975b546612b4b7888df88a7edcf1d209
Jon Parker <jonathanparkeremail@gmail.com>
2022-10-18 09:16:45 +1100

    feat: adds login and logout links in navigation. adds link to webhooks in navigation when logged in.

--- web/src/layouts/DefaultLayout/DefaultLayout.tsx
+++ web/src/layouts/DefaultLayout/DefaultLayout.tsx
@@ -1,10 +1,12 @@
 import { Link, routes } from '@redwoodjs/router'
+import { useAuth } from '@redwoodjs/auth'
 
 type DefaultLayoutProps = {
   children?: React.ReactNode
 }
 
 const DefaultLayout = ({ children }: DefaultLayoutProps) => {
+  const { isAuthenticated } = useAuth()
   return (
     <>
       <header>
@@ -19,6 +21,15 @@ const DefaultLayout = ({ children }: DefaultLayoutProps) => {
             <li>
               <Link to={routes.about()}>About</Link>
             </li>
+            {isAuthenticated ? (
+              <li>
+                <Link to={routes.webhooks()}>Webhooks</Link>
+              </li>
+            ) : (
+              <li>
+                <Link to={routes.login()}>Login</Link>
+              </li>
+            )}
           </ul>
         </nav>
       </header>
--- web/src/layouts/ScaffoldLayout/ScaffoldLayout.tsx
+++ web/src/layouts/ScaffoldLayout/ScaffoldLayout.tsx
@@ -1,3 +1,4 @@
+import { useAuth } from '@redwoodjs/auth'
 import { Link, routes } from '@redwoodjs/router'
 import { Toaster } from '@redwoodjs/web/toast'
 
@@ -16,15 +17,43 @@ const ScaffoldLayout = ({
   buttonTo,
   children,
 }: LayoutProps) => {
+  const { isAuthenticated, logOut } = useAuth()
   return (
     <div className="rw-scaffold">
       <Toaster toastOptions={{ className: 'rw-toast', duration: 6000 }} />
+
       <header className="rw-header">
         <h1 className="rw-heading rw-heading-primary">
           <Link to={routes[titleTo]()} className="rw-link">
             {title}
           </Link>
         </h1>
+        <nav>
+          <ul>
+            <li>
+              <Link to={routes.home()}>Home</Link>
+            </li>
+            <li>
+              <Link to={routes.about()}>About</Link>
+            </li>
+            {isAuthenticated ? (
+              <>
+                <li>
+                  <Link to={routes.webhooks()}>Webhooks</Link>
+                </li>
+                <li>
+                  <a href="#" onClick={logOut}>
+                    Logout
+                  </a>
+                </li>
+              </>
+            ) : (
+              <li>
+                <Link to={routes.login()}>Login</Link>
+              </li>
+            )}
+          </ul>
+        </nav>
         <Link to={routes[buttonTo]()} className="rw-button rw-button-green">
           <div className="rw-button-icon">+</div> {buttonLabel}
         </Link>
