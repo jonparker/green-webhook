c2741309384cbeeb7de5da067eab2af611faddce
Jon Parker <jonathanparkeremail@gmail.com>
2022-10-18 00:09:50 +1100

    feat: adds dbAuth and user schema

---
+++ api/db/migrations/20221017125220_create_user/migration.sql
@@ -0,0 +1,13 @@
+-- CreateTable
+CREATE TABLE "User" (
+    "id" TEXT NOT NULL PRIMARY KEY,
+    "name" TEXT,
+    "email" TEXT NOT NULL,
+    "hashedPassword" TEXT NOT NULL,
+    "salt" TEXT NOT NULL,
+    "resetToken" TEXT,
+    "resetTokenExpiresAt" DATETIME
+);
+
+-- CreateIndex
+CREATE UNIQUE INDEX "User_email_key" ON "User"("email");
--- web/src/Routes.tsx
+++ web/src/Routes.tsx
@@ -7,7 +7,7 @@
 // 'src/pages/HomePage/HomePage.js'         -> HomePage
 // 'src/pages/Admin/BooksPage/BooksPage.js' -> AdminBooksPage
 
-import { Router, Route, Set } from '@redwoodjs/router'
+import { Private, Router, Route, Set } from '@redwoodjs/router'
 
 import ScaffoldLayout from 'src/layouts/ScaffoldLayout'
 import DefaultLayout from './layouts/DefaultLayout/DefaultLayout'
@@ -15,12 +15,18 @@ import DefaultLayout from './layouts/DefaultLayout/DefaultLayout'
 const Routes = () => {
   return (
     <Router>
-      <Set wrap={ScaffoldLayout} title="Webhooks" titleTo="webhooks" buttonLabel="New Green Webhook" buttonTo="newWebhook">
-        <Route path="/webhooks/new" page={WebhookNewWebhookPage} name="newWebhook" />
-        <Route path="/webhooks/{id}/edit" page={WebhookEditWebhookPage} name="editWebhook" />
-        <Route path="/webhooks/{id}" page={WebhookWebhookPage} name="webhook" />
-        <Route path="/webhooks" page={WebhookWebhooksPage} name="webhooks" />
-      </Set>
+      <Route path="/login" page={LoginPage} name="login" />
+      <Route path="/signup" page={SignupPage} name="signup" />
+      <Route path="/forgot-password" page={ForgotPasswordPage} name="forgotPassword" />
+      <Route path="/reset-password" page={ResetPasswordPage} name="resetPassword" />
+      <Private unauthenticated="home">
+        <Set wrap={ScaffoldLayout} title="Webhooks" titleTo="webhooks" buttonLabel="New Green Webhook" buttonTo="newWebhook">
+          <Route path="/webhooks/new" page={WebhookNewWebhookPage} name="newWebhook" />
+          <Route path="/webhooks/{id}/edit" page={WebhookEditWebhookPage} name="editWebhook" />
+          <Route path="/webhooks/{id}" page={WebhookWebhookPage} name="webhook" />
+          <Route path="/webhooks" page={WebhookWebhooksPage} name="webhooks" />
+        </Set>
+      </Private>
       <Set wrap={DefaultLayout}>
         <Route path="/about" page={AboutPage} name="about" />
         <Route path="/" page={HomePage} name="home" />
