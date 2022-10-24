e68b36cb18fe8feb773358e7d8e8a8551ffb02ae
Jade True <jadetruexo@gmail.com>
2022-10-24 15:07:39 +0100

    Updated Navigation

--- web/src/components/Navigation/Navigation.tsx
+++ web/src/components/Navigation/Navigation.tsx
@@ -14,38 +14,37 @@ const Navigation: React.FC<NavProps> = ({ buttonLabel, buttonTo }) => {
 
   return (
     <nav>
-        <ul className='flex flex-col items-center justify-center w-full mt-12 divide-y-2 border-y-2 md:p-10 lg:flex-row lg:gap-14 md:bg-green divide-green border-green'>
-          <Link to={routes.home()}>
-            <ListItem>Home</ListItem>
-          </Link>
-          <Link to={routes.about()}>
-            <ListItem>About</ListItem>
-          </Link>
-          {hasRole('admin') && (
-          <Link to={routes.users()}>
-            <ListItem>Admin</ListItem>
-          </Link>
-          )}
-          {isAuthenticated ? (
-            <>
-              <Link to={routes.webhooks()}>
-                <ListItem>Webhooks</ListItem>
-              </Link>
-               <a href="#" onClick={logOut}>
-               <ListItem>Logout</ListItem>
-              </a>
-              <Link to={buttonTo}>
-                <ListItem styles="bg-emerald-800 text-white">+ {buttonLabel}</ListItem>
-              </Link>
-            </>
-          ) : (
-            <Link to={routes.login()}>
-              <ListItem>Login</ListItem>
+      <ul className='flex flex-col justify-center w-full mt-12 divide-y-2 md:items-center lg:flex-row border-y-2 md:p-10 xl:gap-14 md:bg-green divide-green border-green'>
+        <Link to={routes.home()}>
+          <ListItem>Home</ListItem>
+        </Link>
+        <Link to={routes.about()}>
+          <ListItem>About</ListItem>
+        </Link>
+        {hasRole('admin') && (
+        <Link to={routes.users()}>
+          <ListItem>Admin</ListItem>
+        </Link>
+        )}
+        {isAuthenticated ? (
+          <>
+            <Link to={routes.webhooks()}>
+              <ListItem>Webhooks</ListItem>
             </Link>
-          )}
-        </ul>
-
-      </nav>
+              <a href="#" onClick={logOut}>
+              <ListItem>Logout</ListItem>
+            </a>
+            <Link to={buttonTo}>
+              <ListItem styles="bg-emerald-800 text-white">+ {buttonLabel}</ListItem>
+            </Link>
+          </>
+        ) : (
+          <Link to={routes.login()}>
+            <ListItem>Login</ListItem>
+          </Link>
+        )}
+      </ul>
+    </nav>
   )
 }
 
