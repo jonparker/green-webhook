e0d9ae4752c847f8694b461b9e454ee691ecc179
Abdul Samad <62374784+mr-loop-1@users.noreply.github.com>
2022-11-04 19:19:12 +0530

    Endpoint decision algorithm (#46)
    * added limiting indexes
    
    * make all emssion list
    
    * core calculation
    
    * return correct
    
    * add execption
    
    * bug fix in object
    
    * spelling
    
    * todo
    
    * argument pass
    
    * add debug
    
    * Added CACHE support
    
    * bug fix
    
    * feat: working schedule webhook
    
    Co-authored-by: Jon Parker <jonathanparkeremail@gmail.com>

--- api/src/functions/webhook/webhook.ts
+++ api/src/functions/webhook/webhook.ts
@@ -7,7 +7,7 @@ import { endpointHelper } from 'src/lib/endpointHelper'
 import { logger } from 'src/lib/logger'
 import { forwardWebhook } from 'src/services/forwardWebhook/forwardWebhook'
 
-const cache = new NodeCache({ stdTTL: 60 })
+const cache = new NodeCache()
 
 /**
  * The handler function is your code that processes http request events.
@@ -69,42 +69,67 @@ export const handler = async (event: APIGatewayEvent, context: Context) => {
       const endpoints = endpointHelper.decode(webhook.destinationEndpoints)
 
       let bestEndpoint = null
+      let bestTimestamp = null
 
       if (webhook.maxDelaySeconds < 0) {
         return invalidWebhookId()
       } else {
         // Check Carbon Aware SDK for best location
         const locations = endpoints.map((e) => e.location)
-        const bestLocationInfo = await getLocationWithLowestEmissions(locations)
+        const bestLocationInfo = await getLocationWithLowestEmissions(
+          locations,
+          webhook.maxDelaySeconds
+        )
         logger.info('Best endpoint is', bestLocationInfo)
         const foundBestEndpoint = endpoints.find(
           (endpoint) => endpoint.location === bestLocationInfo.location
         )
         bestEndpoint = foundBestEndpoint.uri
+        bestTimestamp = bestLocationInfo.timestamp
       }
 
       if (bestEndpoint !== null) {
-        const endpointResponse = await forwardWebhook({
-          httpMethod: event.httpMethod,
-          payload: event.body,
-          endpoint: bestEndpoint,
-          queryParams: event.queryStringParameters
-        })
-        logger.info(`Endpoint response`, endpointResponse)
+        if (webhook.maxDelaySeconds === 0) {
+          const endpointResponse = await forwardWebhook({
+            httpMethod: event.httpMethod,
+            payload: event.body,
+            endpoint: bestEndpoint,
+            queryParams: event.queryStringParameters,
+            headers: event.headers,
+          })
+          logger.info(`Endpoint response`, endpointResponse)
 
-        if (endpointResponse.statusCode === 200) {
-          // 6. Update invocations count on webhook.
-          await db.webhook.update({
-            where: { id: webhookId },
-            data: {
-              invocations: {
-                increment: 1,
+          if (endpointResponse.statusCode === 200) {
+            // 6. Update invocations count on webhook.
+            await db.webhook.update({
+              where: { id: webhookId },
+              data: {
+                invocations: {
+                  increment: 1,
+                },
               },
+            })
+          }
+          return successfulResponse(endpointResponse)
+        } else {
+          // Schedule
+          await db.scheduledInvocation.create({
+            data: {
+              webhookId: webhook.id.toString(),
+              httpMethod: event.httpMethod,
+              payload: event.body,
+              endpointUri: bestEndpoint,
+              queryParams: event.queryStringParameters.toString(),
+              invokeEndpointAt: bestTimestamp,
+              isCompleted: false,
+              successfulResponse: false,
             },
           })
+          return successfulResponse({
+            statusCode: 200,
+            message: 'Scheduled',
+          })
         }
-
-        return successfulResponse(endpointResponse)
       } else {
         return invalidWebhookId()
       }
@@ -114,56 +139,216 @@ export const handler = async (event: APIGatewayEvent, context: Context) => {
   return invalidWebhookId()
 }
 
-type LocationInfo = {
-  location: string
-  carbonAwareInfo: {
-    location?: string
-    time?: Date
-    rating?: number
-    duration?: string
+// type LocationInfo = {
+//   location: string
+//   carbonAwareInfo: {
+//     location?: string
+//     time?: Date
+//     rating?: number
+//     duration?: string
+//   }
+// }
+const getCombination = (allEmissions, delayIndex, durationWindow) => {
+  const allEmissionsSize = allEmissions.length
+
+  if (durationWindow > allEmissionsSize) {
+    throw new Error(
+      `Error message: Forcast not available yet, Status code: 404}`
+    )
   }
+
+  let currentTotalCarbs = 0,
+    bestCurrentTimestamp,
+    minCurrentTotalCarbs = 10000
+
+  for (let i = 0; i < durationWindow; i++) {
+    currentTotalCarbs += allEmissions[i].value
+  }
+
+  if (currentTotalCarbs < minCurrentTotalCarbs) {
+    minCurrentTotalCarbs = currentTotalCarbs
+    bestCurrentTimestamp = allEmissions[0].timestamp
+  }
+
+  for (
+    let i = 0, j = durationWindow;
+    i < delayIndex && j < allEmissionsSize;
+    i++, j++
+  ) {
+    currentTotalCarbs -= allEmissions[i].value
+    currentTotalCarbs += allEmissions[j].value
+
+    if (currentTotalCarbs < minCurrentTotalCarbs) {
+      minCurrentTotalCarbs = currentTotalCarbs
+      bestCurrentTimestamp = allEmissions[i + 1].timestamp
+    }
+  }
+
+  return [bestCurrentTimestamp, minCurrentTotalCarbs]
 }
 
-const getLocationWithLowestEmissions = async (locations: string[]) => {
-  const locationInfo = new Array<LocationInfo>()
+const getLocationWithLowestEmissions = async (
+  locations: string[],
+  maxDelaySeconds: number,
+  lastRecordedDuration = 700
+) => {
   const baseUri = process.env.CARBON_AWARE_API_BASE_URI
   if (!baseUri) {
     throw new Error("Missing 'CARBON_AWARE_API_BASE_URI' environment variable")
   }
+
+  const maxDelayTime = {
+    hours: maxDelaySeconds / 3600,
+    minutes: (maxDelaySeconds % 3600) / 60,
+    seconds: (maxDelaySeconds % 3600) % 60,
+  }
+
+  const estimatedTime = {
+    hours: lastRecordedDuration / 3600,
+    minutes: (lastRecordedDuration % 3600) / 60,
+    seconds: (lastRecordedDuration % 3600) % 60,
+  }
+
+  const durationWindow = Math.ceil(
+    (Math.floor(estimatedTime.hours) * 60 + Math.floor(estimatedTime.minutes)) /
+      5
+  )
+  console.log('windwo size ', durationWindow)
+
+  // console.log((maxDelayTime.hours * 60) + maxDelayTime.minutes)
+  const delayIndex = Math.floor(
+    (Math.floor(maxDelayTime.hours) * 60 + Math.floor(maxDelayTime.minutes)) / 5
+  )
+  console.log('delay index ', delayIndex)
+
+  let bestLocation,
+    bestTimestamp,
+    minTotalCarbs = 10000
+
   const api = new CarbonAwareApi(baseUri)
+
   for (const location of locations) {
     if (cache.has(location) === true) {
-      console.log('Cache hit for location', location)
-      locationInfo.push(cache.get(location))
+      console.log('cache hit for location ', location)
+
+      const cachedData: {
+        allEmissions
+        durationWindow
+        delayIndex
+        minCurrentTotalCarbs
+        bestCurrentTimestamp
+      } = cache.get(location)
+
+      if (
+        cachedData.durationWindow === durationWindow &&
+        cachedData.delayIndex === delayIndex
+      ) {
+        console.log('cache matched for location ', location)
+
+        if (cachedData.minCurrentTotalCarbs < minTotalCarbs) {
+          minTotalCarbs = cachedData.minCurrentTotalCarbs
+          bestTimestamp = cachedData.bestCurrentTimestamp
+          bestLocation = location
+        }
+      } else {
+        console.log('cache not matched for location ', location)
+        const allEmissions = cachedData.allEmissions
+
+        const [bestCurrentTimestamp, minCurrentTotalCarbs] = getCombination(
+          allEmissions,
+          delayIndex,
+          durationWindow
+        )
+
+        if (minCurrentTotalCarbs < minTotalCarbs) {
+          minTotalCarbs = minCurrentTotalCarbs
+          bestTimestamp = bestCurrentTimestamp
+          bestLocation = location
+        }
+      }
     } else {
-      console.log('Cache miss for location', location)
+      console.log('cache Miss for location ', location)
+
       try {
-        const emissionsForLocation =
-          await api.getEmissionsDataForLocationByTime(location)
-        if (emissionsForLocation.response.statusCode === 200) {
-          {
-            locationInfo.push({
-              location,
-              carbonAwareInfo: emissionsForLocation.body[0],
-            })
-            cache.set(location, {
-              location,
-              carbonAwareInfo: emissionsForLocation.body[0],
-            })
-          }
-        } else {
+        const emissionForcast = await api.getCurrentForecastData(
+          Array.of(location)
+        )
+        const currentEmission = await api.getEmissionsDataForLocationByTime(
+          location
+        )
+
+        if (emissionForcast.response.statusCode !== 200) {
           throw new Error(
-            `Error message: ${emissionsForLocation.response.statusMessage}, Status code: ${emissionsForLocation.response.statusCode}`
+            `Error message: ${emissionForcast.response.statusMessage}, Status code: ${emissionForcast.response.statusCode}`
           )
         }
+        if (currentEmission.response.statusCode !== 200) {
+          throw new Error(
+            `Error message: ${currentEmission.response.statusMessage}, Status code: ${currentEmission.response.statusCode}`
+          )
+        }
+
+        const newCurrentEmission = {
+          ...currentEmission.body[0],
+          timestamp: currentEmission.body[0].time,
+          value: currentEmission.body[0].rating,
+        }
+        delete newCurrentEmission.time
+        delete newCurrentEmission.rating
+
+        const allEmissions = [
+          newCurrentEmission,
+          ...emissionForcast.body[0].forecastData,
+        ]
+
+        const [bestCurrentTimestamp, minCurrentTotalCarbs] = getCombination(
+          allEmissions,
+          delayIndex,
+          durationWindow
+        )
+
+        if (minCurrentTotalCarbs < minTotalCarbs) {
+          minTotalCarbs = minCurrentTotalCarbs
+          bestTimestamp = bestCurrentTimestamp
+          bestLocation = location
+        }
+
+        const curr = {
+          allEmissions,
+          durationWindow,
+          delayIndex,
+          minCurrentTotalCarbs,
+          bestCurrentTimestamp,
+        }
+
+        // for 1:42:31
+        // stdttl = 180-31 = 149 seconds (2:29)
+        // hence expiry at 1:45:00
+        const currTime = new Date()
+        cache.set(
+          location,
+          curr,
+          (5 - (currTime.getMinutes() % 5)) * 60 - currTime.getSeconds()
+        )
+        console.log('cache SET for location ', location)
       } catch (error) {
         console.log('Error getting emissions for location', location, error)
       }
     }
   }
-  return locationInfo.sort(
-    (a, b) => a.carbonAwareInfo.rating - b.carbonAwareInfo.rating
-  )[0]
+  const bestCombination = {
+    location: bestLocation,
+    timestamp: bestTimestamp,
+    carbonRatingForDuration: Math.floor(minTotalCarbs / durationWindow),
+  }
+  console.log(
+    'Best location is ',
+    bestLocation,
+    ' at timestamp ',
+    bestTimestamp
+  )
+
+  return bestCombination
 }
 
 const invalidWebhookId = () => {
